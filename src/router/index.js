03d330db2a51758550ff5cf813b333b7aed92a4a
Kristofer <kristoferhenriquez@gmail.com>
2020-08-31 20:57:12 -0500

    added Games Index page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from '../views/Home.vue';
+import GamesIndex from '../views/GamesIndex.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/games',
+    name: 'GamesIndex',
+    component: GamesIndex
   },
   {
     path: '/about',
@@ -18,12 +24,12 @@ Vue.use(VueRouter)
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
-]
+];
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
---
+++ src/views/GamesIndex.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="games">
+    <h1>{{ message }}</h1>
+    <div v-for="game in games">
+      <h2>{{ game.title }}</h2>
+      <br>
+      <img v-bind:src="game.game_url" width="400" height="200">
+    </div>
+  </div>
+</template>
+
+<style>
+</style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function() {
+    return {
+      message: "Choose below",
+      games: []
+    };
+  },
+  created: function() {
+    this.indexGames();
+  },
+  methods: {
+    indexGames: function() {
+      axios.get("/api/games").then(response => {
+        console.log("response.data");
+        this.games = response.data;
+      });
+    }
+  }
+};
+</script>
