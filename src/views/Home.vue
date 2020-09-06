ce26681293ca9c4b671353747fe5553ad768bb43
Kristofer <kristoferhenriquez@gmail.com>
2020-09-06 02:47:25 -0500

    implemented a filter option for universals in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <input type="text" v-model="searchTerm" list="titles">
+    <datalist id="titles">
+      <option v-for="universal in universals">{{universal.title}}</option>
+    </datalist>
+    <!-- <div v-for="universal in universals"> -->
+    <div v-for="universal in filterBy(universals, searchTerm, 'title')">
+      {{universal.title}}
+      {{universal.body}}
+    </div>
   </div>
 </template>
 
@@ -8,13 +17,28 @@
 </style>
 
 <script>
+import axios from 'axios';
+import Vue2Filters from 'vue2-filters';
+
 export default {
   data: function() {
     return {
-      message: "Super cool Crosso logo goes here"
+      message: "Crosso",
+      universals: [],
+      searchTerm: ""
     };
   },
-  created: function() {},
-  methods: {}
+  created: function() {
+    this.indexUniversals();
+  },
+  methods: {
+    indexUniversals: function() {
+      axios.get("/api/universals").then(response => {
+        console.log("response.data");
+        this.universals = response.data;
+      });
+    }
+  },
+  mixins: [Vue2Filters.mixin]
 };
 </script>
