456afe8ec57c73b2d6e69ee91fa70153054f6b38
Kristofer <kristoferhenriquez@gmail.com>
2020-09-05 22:55:22 -0500

    Implemented vue video embed for youtube videoss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,18 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import axios from "axios";
+import Embed from 'v-video-embed';
+import LazyYoutubeVideo from 'vue-lazy-youtube-video';
+import VueYoutubePlayer from 'vue-youtube-player';
+import VueYouTubeEmbed from 'vue-youtube-embed';
+
+Vue.use(VueYouTubeEmbed);
+
+
+Vue.component('LazyYoutubeVideo', LazyYoutubeVideo);
+
+// global register
+Vue.use(Embed);
 
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
--- src/views/DbFighterz.vue
+++ src/views/DbFighterz.vue
@@ -1,29 +1,64 @@
 <template>
   <div class="DbFighterz">
     <img v-bind:src="game.game_url" width="400" height="200">
-    <h2> DLC </h2>
-    <h2> Patch Notes </h2>
-    <h2> Stages </h2>
-    <h2> Characters </h2>
+    <h2><a v-bind:href="`/dbfighterz/dlc/`"> DLC </a></h2>
+    <h2><a v-bind:href="`/dbfighterz/stages/`"> stages </a></h2>
+    <h2><a v-bind:href="`/dbfighterz/patch_notes/`"> Patch Notes </a></h2>
+    <h2><a v-bind:href="`/dbfighterz/characters/`"> Characters </a></h2>
+    <br>
+    <br>
     <h1>{{ message }}</h1>
+    <h2>Combos</h2>
+    <div v-for="combo in combos">
+      <video-embed v-bind:src=combo.combo_video></video-embed>
+      {{ combo.notation }}
+    </div>
+    <h2> Tutorials </h2>
+    <div v-for="tutorial in tutorials">
+      <h3>{{tutorial.title}}</h3>
+      <br>
+      {{tutorial.body}}
+    </div>
+    <h2> Tips </h2>
+    <div v-for="tip in tips">
+      {{tip.tips_list}}
+    </div>
+    <h2> Tier List </h2>
+    <div v-for="tier_list in tier_lists">
+    
+      <img v-bind:src="tier_list.list" width="800" height="400">
+    </div>
   </div>
 </template>
 
 <style>
+
 </style>
 
 <script>
 import axios from "axios";
+import Embed from 'v-video-embed';
+import LazyYoutubeVideo from 'vue-lazy-youtube-video';
+import VueYoutubePlayer from 'vue-youtube-player';
+import VueYouTubeEmbed from 'vue-youtube-embed';
 
 export default {
   data: function() {
     return {
       message: "See your content below",
-      game: []
+      game: [],
+      combos: {},
+      tips: [],
+      tutorials: [],
+      tier_lists: []
     };
   },
   created: function() {
     this.indexGames();
+    this.indexTierLists();
+    this.indexCombos();
+    this.indexTips();
+    this.indexTutorials();
 
   },
   methods: {
@@ -32,7 +67,31 @@ export default {
         console.log("response.data");
         this.game = response.data;
       });
-    }
-  }
+    },
+    indexCombos: function() {
+      axios.get("api/combos").then(response => {
+        console.log("response.data");
+        this.combos = response.data;
+      });
+    },
+    indexTips: function() {
+      axios.get("api/tips").then(response => {
+        console.log("response.data");
+        this.tips = response.data;
+      });
+    },
+    indexTutorials: function() {
+      axios.get("api/tutorials").then(response => {
+        console.log("response.data");
+        this.tutorials = response.data;
+      });
+    },
+    indexTierLists: function() {
+      axios.get("api/tier_lists").then(response => {
+        console.log("response.data");
+        this.tier_lists = response.data;
+      });
+    },
+  },
 };
 </script>
